41f1d201538642e02d1e40d338589a047a4f435d
Gurtubay <josegurtubaymoreno@gmail.com>
2022-11-30 18:42:45 -0600

    Los coches ya no chocan

--- Revision3/scriptsServer/agent.py
+++ Revision3/scriptsServer/agent.py
@@ -21,8 +21,115 @@ class Car(Agent):
         self.rutas = rutas
         self.Waze = self.rutas.camino(self.pos,self.destination)
         self.reachVertice = 0
+        self.vision=[(0, 0),(0, 0),(0, 0),(0, 0),(0, 0),(0, 0),(0, 0),(0, 0),(0, 0),(0, 0),(0, 0)]
+        self.breakLights=False
         #self.knowledge1=[(pos),obj] <- SENSORES
-
+        
+    def justMoveIf(self):
+        outIndex=False
+        outIndex1=False
+        outIndex2=False
+        if self.vision[0][0]>=0 and self.vision[0][0] <=23 and self.vision[0][1] >=0 and self.vision[0][1] <=24:
+            position =self.model.grid.get_cell_list_contents(self.vision[0])
+            cocheDelante=[obj for obj in position if isinstance(obj,Car)]
+            outIndex=True
+            
+        if self.vision[3][0]>=0 and self.vision[3][0] <=23 and self.vision[3][1] >=0 and self.vision[3][1] <=24:
+            position2 =self.model.grid.get_cell_list_contents(self.vision[3])
+            cocheDelante2=[obj for obj in position2 if isinstance(obj,Car)]
+            outIndex1=True
+            
+        if self.vision[6][0]>=0 and self.vision[6][0] <=23 and self.vision[6][1] >=0 and self.vision[6][1] <=24:
+            position3 =self.model.grid.get_cell_list_contents(self.vision[6])
+            cocheDelante3=[obj for obj in position3 if isinstance(obj,Car)]
+            outIndex2=True
+            
+        if outIndex and outIndex1 and outIndex2:    
+            if len(cocheDelante)==0 and (len(cocheDelante2)==0 or cocheDelante2[0].lastDirection != "Down") and (len(cocheDelante3)==0 or cocheDelante3[0].lastDirection != "Up") and self.lastDirection=="Right":
+                return True
+            elif len(cocheDelante)==0 and (len(cocheDelante2)==0 or cocheDelante2[0].lastDirection != "Down") and (len(cocheDelante3)==0 or cocheDelante3[0].lastDirection != "Up") and self.lastDirection=="Left":
+                return True
+            elif len(cocheDelante)==0 and self.lastDirection=="Up":
+                return True
+            elif len(cocheDelante)==0 and self.lastDirection=="Down":
+                return True
+            
+        elif outIndex and outIndex1:
+            if len(cocheDelante)==0 and (len(cocheDelante2)==0 or cocheDelante2[0].lastDirection != "Down") and self.lastDirection=="Right":
+                return True
+            elif len(cocheDelante)==0 and (len(cocheDelante2)==0 or cocheDelante2[0].lastDirection != "Down") and self.lastDirection=="Left":
+                return True
+            elif len(cocheDelante)==0 and self.lastDirection=="Up":
+                return True
+            elif len(cocheDelante)==0 and self.lastDirection=="Down":
+                return True
+            
+        elif outIndex and outIndex2:
+            if len(cocheDelante)==0 and (len(cocheDelante3)==0 or cocheDelante3[0].lastDirection != "Up") and self.lastDirection=="Right":
+                return True
+            elif len(cocheDelante)==0 and (len(cocheDelante3)==0 or cocheDelante3[0].lastDirection != "Up") and self.lastDirection=="Left":
+                return True
+            elif len(cocheDelante)==0 and self.lastDirection=="Up":
+                return True
+            elif len(cocheDelante)==0 and self.lastDirection=="Down":
+                return True
+        
+        
+    def setVision(self):
+        if self.lastDirection=="Left":
+            self.vision[0]=(self.pos[0]-1,self.pos[1])
+            self.vision[1]=(self.pos[0]-2,self.pos[1])
+            self.vision[2]=(self.pos[0]-3,self.pos[1])
+            self.vision[3]=(self.pos[0]-1,self.pos[1]+1)
+            self.vision[4]=(self.pos[0]-2,self.pos[1]+1)
+            self.vision[5]=(self.pos[0]-3,self.pos[1]+1)
+            self.vision[6]=(self.pos[0]-1,self.pos[1]-1)
+            self.vision[7]=(self.pos[0]-2,self.pos[1]-1)
+            self.vision[8]=(self.pos[0]-3,self.pos[1]-1)
+            self.vision[9]=(self.pos[0],self.pos[1]+1)
+            self.vision[10]=(self.pos[0],self.pos[1]-1)
+            
+        elif self.lastDirection=="Right":
+            print("Reajuste")
+            self.vision[0]=(self.pos[0]+1,self.pos[1])
+            self.vision[1]=(self.pos[0]+2,self.pos[1])
+            self.vision[2]=(self.pos[0]+3,self.pos[1])
+            self.vision[3]=(self.pos[0]+1,self.pos[1]+1)
+            self.vision[4]=(self.pos[0]+2,self.pos[1]+1)
+            self.vision[5]=(self.pos[0]+3,self.pos[1]+1)
+            self.vision[6]=(self.pos[0]+1,self.pos[1]-1)
+            self.vision[7]=(self.pos[0]+2,self.pos[1]-1)
+            self.vision[8]=(self.pos[0]+3,self.pos[1]-1)
+            self.vision[9]=(self.pos[0],self.pos[1]+1)
+            self.vision[10]=(self.pos[0],self.pos[1]-1)
+            
+        elif self.lastDirection=="Up":
+            self.vision[0]=(self.pos[0],self.pos[1]+1)
+            self.vision[1]=(self.pos[0],self.pos[1]+2)
+            self.vision[2]=(self.pos[0],self.pos[1]+3)
+            self.vision[3]=(self.pos[0]+1,self.pos[1]+1)
+            self.vision[4]=(self.pos[0]+1,self.pos[1]+2)
+            self.vision[5]=(self.pos[0]+1,self.pos[1]+3)
+            self.vision[6]=(self.pos[0]-1,self.pos[1]+1)
+            self.vision[7]=(self.pos[0]-1,self.pos[1]+2)
+            self.vision[8]=(self.pos[0]-1,self.pos[1]+3)
+            self.vision[9]=(self.pos[0]+1,self.pos[1])
+            self.vision[10]=(self.pos[0]-1,self.pos[1])
+            
+        elif self.lastDirection=="Down":
+            self.vision[0]=(self.pos[0],self.pos[1]-1)
+            self.vision[1]=(self.pos[0],self.pos[1]-2)
+            self.vision[2]=(self.pos[0],self.pos[1]-3)
+            self.vision[3]=(self.pos[0]+1,self.pos[1]-1)
+            self.vision[4]=(self.pos[0]+1,self.pos[1]-2)
+            self.vision[5]=(self.pos[0]+1,self.pos[1]-3)
+            self.vision[6]=(self.pos[0]-1,self.pos[1]-1)
+            self.vision[7]=(self.pos[0]-1,self.pos[1]-2)
+            self.vision[8]=(self.pos[0]-1,self.pos[1]-3)
+            self.vision[9]=(self.pos[0]+1,self.pos[1])
+            self.vision[10]=(self.pos[0]-1,self.pos[1])
+            
+        
     def move(self):
         """ 
         Determines if the agent can move in the direction that was chosen
@@ -40,9 +147,9 @@ class Car(Agent):
         position =self.model.grid.get_cell_list_contents([self.pos])
         direccionRoad=[obj for obj in position if isinstance(obj,Road)]
         goOrStop = [obj for obj in position if isinstance(obj,Traffic_Light)]
-        print(str(self.unique_id) +"|"+ str(self.Waze))
-        print(str(self.unique_id) +"|" +str(self.pos))
-        print(str(self.unique_id)+ "|" +str(self.Waze[0][self.reachVertice]))
+        #print(str(self.unique_id) +"|"+ str(self.Waze))
+        #print(str(self.unique_id) +"|" +str(self.pos))
+        #print(str(self.unique_id)+ "|" +str(self.Waze[0][self.reachVertice]))
         if self.Waze[0][self.reachVertice] == self.destination:
             if self.pos[0]==self.destination[0]+1 and self.pos[1]==self.destination[1]+1:
                 self.model.grid.move_agent(self,(x0-1,y0-1))
@@ -53,23 +160,27 @@ class Car(Agent):
             elif self.pos[0]==self.destination[0]+1 and self.pos[1]==self.destination[1]-1:
                 self.model.grid.move_agent(self,(x0-1,y0+1))
             else:
-                print("Ultima recta!!!!!!")
+                #print("Ultima recta!!!!!!")
                 if (self.pos[0]==self.Waze[0][self.reachVertice][0]-1 or self.pos[0]==self.Waze[0][self.reachVertice][0]+1) and self.pos[1]-self.Waze[0][self.reachVertice][1]<0:
-                    self.model.grid.move_agent(self,(x0,y0+1))
-                    print("Finalmente hacia arriba")
-                    #self.lastDirection="Up"
+                    if self.justMoveIf():
+                        self.model.grid.move_agent(self,(x0,y0+1))
+                    #print("Finalmente hacia arriba")
+                    self.lastDirection="Up"
                 elif (self.pos[0]==self.Waze[0][self.reachVertice][0]-1 or self.pos[0]==self.Waze[0][self.reachVertice][0]+1) and self.pos[1]-self.Waze[0][self.reachVertice][1]>0:
-                    self.model.grid.move_agent(self,(x0,y0-1))
-                    print("Finalmente hacia abajo")
-                    #self.lastDirection="Down"
+                    if self.justMoveIf():
+                        self.model.grid.move_agent(self,(x0,y0-1))
+                    #print("Finalmente hacia abajo")
+                    self.lastDirection="Down"
                 elif (self.pos[1]==self.Waze[0][self.reachVertice][1]-1 or self.pos[1]==self.Waze[0][self.reachVertice][1]+1) and self.pos[0]-self.Waze[0][self.reachVertice][0]>0:
-                    self.model.grid.move_agent(self,(x0-1,y0))
-                    print("Finalmente hacia Izq")
-                    #self.lastDirection="Left"
+                    if self.justMoveIf():
+                        self.model.grid.move_agent(self,(x0-1,y0))
+                    #print("Finalmente hacia Izq")
+                    self.lastDirection="Left"
                 elif (self.pos[1]==self.Waze[0][self.reachVertice][1]-1 or self.pos[1]==self.Waze[0][self.reachVertice][1]+1) and self.pos[0]-self.Waze[0][self.reachVertice][0]<0:
-                    self.model.grid.move_agent(self,(x0+1,y0))
-                    print("Finalmente hacia Der")
-                    #self.lastDirection="Right"
+                    if self.justMoveIf():
+                        self.model.grid.move_agent(self,(x0+1,y0))
+                    #print("Finalmente hacia Der")
+                    self.lastDirection="Right"
                 elif (self.pos[0]==self.Waze[0][self.reachVertice][0]-2 or self.pos[0]==self.Waze[0][self.reachVertice][0]+2) and self.pos[1]-self.Waze[0][self.reachVertice][1]<0:
                     if self.pos[0]-self.Waze[0][self.reachVertice][0]>0:
                         self.model.grid.move_agent(self,(x0-1,y0))
@@ -100,49 +211,57 @@ class Car(Agent):
                 self.reachVertice+=1
             if self.pos[0]==self.Waze[0][self.reachVertice][0]:
                 if self.pos[1]-self.Waze[0][self.reachVertice][1]>0:
-                    print("Enttra para moverse abajo de:")
-                    print(self.pos)
-                    self.model.grid.move_agent(self,(x0,y0-1))
+                    #print("Enttra para moverse abajo de:")
+                    #print(self.pos)
+                    if self.justMoveIf():
+                        self.model.grid.move_agent(self,(x0,y0-1))
                     self.lastDirection="Down"
-                    print("Llego a ")
-                    print(self.pos)
+                    #print("Llego a ")
+                    #print(self.pos)
                 elif self.pos[1]-self.Waze[0][self.reachVertice][1]<0:
-                    print("Enttra para moverse arriba de:")
-                    print(self.pos)
-                    self.model.grid.move_agent(self,(x0,y0+1))
+                    #print("Enttra para moverse arriba de:")
+                    #print(self.pos)
+                    if self.justMoveIf():
+                        self.model.grid.move_agent(self,(x0,y0+1))
                     self.lastDirection="Up"
-                    print("Llego a ")
-                    print(self.pos)
+                    #print("Llego a ")
+                    #print(self.pos)
             elif self.pos[1]==self.Waze[0][self.reachVertice][1]:
                 if self.pos[0]-self.Waze[0][self.reachVertice][0]>0:
-                    print("Enttra para moverse izq de:")
-                    print(self.pos)
-                    self.model.grid.move_agent(self,(x0-1,y0))
+                    #print("Enttra para moverse izq de:")
+                    #print(self.pos)
+                    if self.justMoveIf():
+                        self.model.grid.move_agent(self,(x0-1,y0))
                     self.lastDirection="Left"
-                    print("Llego a ")
-                    print(self.pos)
+                    #print("Llego a ")
+                    #print(self.pos)
                 elif self.pos[0]-self.Waze[0][self.reachVertice][0]<0:
-                    print("Enttra para moverse DER de:")
-                    print(self.pos)
-                    self.model.grid.move_agent(self,(x0+1,y0))
+                    #print("Enttra para moverse DER de:")
+                    #print(self.pos)
+                    if self.justMoveIf():
+                        self.model.grid.move_agent(self,(x0+1,y0))
                     self.lastDirection="Right"
-                    print("Aqui se movio")
-                    print(self.pos)
+                    #print("Aqui se movio")
+                    #print(self.pos)
                 
         elif len(direccionRoad)>0:
             #Direcciones para Left,Right,Up y Down
             if direccionRoad[0].direction=="Left":
-                self.model.grid.move_agent(self,(x0-1,y0))
-                self.lastDirection="Left"
+                if self.justMoveIf():
+                    self.model.grid.move_agent(self,(x0-1,y0))
+                    self.lastDirection="Left"
             elif direccionRoad[0].direction=="Right":
-                self.model.grid.move_agent(self,(x0+1,y0))
-                self.lastDirection="Right"
+                if self.justMoveIf():
+                    self.model.grid.move_agent(self,(x0+1,y0))
+                    self.lastDirection="Right"
             elif direccionRoad[0].direction=="Up":
-                self.model.grid.move_agent(self,(x0,y0+1))
-                self.lastDirection="Up"
+                if self.justMoveIf():
+                    self.model.grid.move_agent(self,(x0,y0+1))
+                    self.lastDirection="Up"
             elif direccionRoad[0].direction=="Down":
-                self.model.grid.move_agent(self,(x0,y0-1))
-                self.lastDirection="Down"
+                if self.justMoveIf():
+                    self.model.grid.move_agent(self,(x0,y0-1))
+                    self.lastDirection="Down"
                 
             #if self.pos == self.Waze[0][self.reachVertice]:
              #   self.reachVertice+=1
@@ -151,27 +270,18 @@ class Car(Agent):
         else:
             #LastDir para Left, Right, Up, Down
             if self.lastDirection=="Left" and goOrStop[0].state:
-                self.model.grid.move_agent(self,(x0-1,y0))
+                if self.justMoveIf():
+                    self.model.grid.move_agent(self,(x0-1,y0))
             elif self.lastDirection=="Right" and goOrStop[0].state:
-                self.model.grid.move_agent(self,(x0+1,y0))
+                if self.justMoveIf():
+                    self.model.grid.move_agent(self,(x0+1,y0))
             elif self.lastDirection=="Up" and goOrStop[0].state:
-                self.model.grid.move_agent(self,(x0,y0+1))
+                if self.justMoveIf():
+                    self.model.grid.move_agent(self,(x0,y0+1))
             elif self.lastDirection=="Down" and goOrStop[0].state:
-                self.model.grid.move_agent(self,(x0,y0-1))
-        """
-        if x0 != xf:
-            direccion = -1 if xf - x0 < 0 else 1 #direccion
-            self.model.grid.move_agent(self,(x0 + direccion,y0))
-            #self.model.grid.move_agent(self,(x + direccion,y))
-            return
-        
-        elif y0 != 1:
-            direccion = -1 if 1 - y0 < 0 else 1 #direccion
-            self.model.grid.move_agent(self,(x0,y0 + direccion))
-            return
-       
-        self.model.grid.move_agent(self)
-        """
+                if self.justMoveIf():
+                    self.model.grid.move_agent(self,(x0,y0-1))
+
     def choque(self):
         a = 0
         #if self.pos == next_agent.pos:
@@ -181,6 +291,7 @@ class Car(Agent):
         """ 
         Determines the new direction it will take, and then moves
         """
+        self.setVision()
         print(self.destination)
         if self.pos==self.destination:
             ##Aqui se destruye el objeto
@@ -202,6 +313,7 @@ class Traffic_Light(Agent):
             state: Whether the traffic light is green or red
             timeToChange: After how many step should the traffic light change color 
         """
+        
         self.state = state
         self.timeToChange = timeToChange
 
